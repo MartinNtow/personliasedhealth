bc33650c6799e18bd88dea266e8c181cc09fdddd
legion1z <ntow020@gmail.com>
2024-05-16 00:01:14 +0000

    designed the navbar

---
+++ src/app/Healthpage/page.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>Personalized treatment</div>
+  )
+}
+
+export default page
--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -1,11 +1,25 @@
 import React from 'react'
 import styles from "../styles/navbar.module.css"
+import Link from 'next/link'
 
 const Navbar = () => {
   return (
     <div className = {styles.container}>
-      this is the navbar
+       
+       <div className={styles.appname}>
+        <Link href = "/Home" className= {styles.link1}>
+         <h1 >TailorCare</h1>
+        </Link>
+       </div>
+
+       <div className= {styles.link}>
+        <Link href = "/Healthpage" className={styles.nav}>Personalized Health & Wellness</Link>
+        <Link href = "/Symptomchecker" className={styles.nav} >AI Symptom Checker</Link>
+       </div>
+       
+      
     </div>
+    
   )
 }
 
