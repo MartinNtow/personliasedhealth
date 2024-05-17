903b82cd06f932f94b74ab1e21c4a49f37dee0ca
legion1z <ntow020@gmail.com>
2024-05-17 10:46:43 +0000

    init

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -4,10 +4,7 @@ import Link from 'next/link'
 
 const Navbar = () => {
   return (
-<<<<<<< HEAD
-    <div>
-      this is the navbars
-=======
+
     <div className = {styles.container}>
        
        <div className={styles.appname}>
@@ -22,7 +19,7 @@ const Navbar = () => {
        </div>
        
       
->>>>>>> 9b48cbcb64057eb41cf712608ef63ab22222df80
+
     </div>
     
   )
